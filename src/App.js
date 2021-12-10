dd55c48566a3a4d4ae75c4922031c3e89c664ea4
Sardor <tojimuradovsardor25@gmail.com>
2021-12-10 13:25:17 +0500

    Silence Companent start : )

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Component } from "react";
+import Silence from './Silence/Silence'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <Silence />
+        </header>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/Silence/Silence.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Silence = (props) => {
+    return (
+        <div className="Container">
+            <header className="App-header">
+            
+            </header>
+        </div>
+    );
+}
+
+export default Silence;
